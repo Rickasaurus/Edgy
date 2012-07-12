6beeab3c5c128ea9a23992ac549e2d8ae159f15c
Richard Minerich <richard.minerich@gmail.com>
2012-07-11 23:34:55 -0400

    Added Union-Find Data Structure in Utility.fs Added Kruskal's Algorithm Some small refactorings
    Signed-off-by: Richard Minerich <richard.minerich@gmail.com>

---
+++ Edgy/Utility.fs
@@ -0,0 +1,35 @@
+﻿module Edgy.Utility
+
+/// Weighted quick-union with path compression
+/// Overall: O((M+N)lg*N) -- M union-find ops on a set of N objects
+/// Implementation from: http://www.cs.princeton.edu/~rs/AlgsDS07/01UnionFind.pdf
+type QuickUWPC private (id: int[], sz: int[]) =
+
+    new (N: int) = 
+        // Parent index, id[i] is parent of i
+        let id = Array.init N (fun i -> i)
+        // Number of elements rooted at i
+        let sz = Array.create N 1
+        QuickUWPC(id, sz) 
+
+    /// Returns the root node which represents the set of the given node
+    member t.Root(i: int) =
+        let mutable q = i
+        while (q <> id.[q]) do 
+            id.[q] <- id.[id.[q]]
+            q <- id.[q] 
+        q
+
+    /// Determine if p and q are in the same set (or if p is in set q)
+    member t.Find(p, q) =
+        t.Root(p) = t.Root(q)
+
+    /// Union: Combine or merge two sets into a single set.
+    member t.Union(p, q) =
+        let i = t.Root(p)
+        let j = t.Root(q)
+        if sz.[i] < sz.[j] then id.[i] <- j; sz.[j] <- sz.[j] + sz.[i]
+        else id.[j] <- i; sz.[i] <- sz.[i] + sz.[j]    
+
+    /// Creates another QuickUWPC with the same internal structure
+    member t.Clone() = new QuickUWPC(Array.copy id, Array.copy sz)
--- Edgy/WeightedGraph.fs
+++ Edgy/WeightedGraph.fs
@@ -1,50 +1,46 @@
 ﻿module Edgy.WeightedDirectedGraph
 #nowarn "62"
 
-type Edge<'a> = 'a * 'a
+#if INTERACTIVE 
+#load "Utility.fs"
+#load "Core.fs"
+#endif
 
-type WeightedPath<'a, 'b when 'a: comparison and 'b: comparison> = 
-    {   
-        /// The last element attached
-        Tail: 'a
-        /// (From --> To) Set
-        Edges: (Edge<'a>, 'b) Map
-    }
-and Weight<'a, 'b when 'a: comparison and 'b: comparison> = 
-    {
-        /// Previous Path
-        Prev: WeightedPath<'a,'b>
-        /// Previous Weight
-        Weight: 'b
-    }
-
-type WeightedPath with
-    /// 'a =| 'n
-    static member (=|) (l: WeightedPath<'a,'b>, n: 'b) : Weight<'a,'b> = { Prev = l; Weight = n }
-    /// 'a <=| 'n
-    static member (<=|) (l: WeightedPath<'a,'b>, n: 'b) : Weight<'a,'b> = { Prev = l; Weight = n }
-
-type Weight with
-    /// 'n |=> 'a
-    static member (|=>) (l: Weight<'a,'b>, r: 'a) : WeightedPath<'a,'b> =
-        { Tail = r; Edges = l.Prev.Edges |> Map.add (l.Prev.Tail, r) l.Weight }
-    /// 'n |= 'a
-    static member (|=) (l: Weight<'a,'b>, r: 'a) : WeightedPath<'a,'b> =
-        { Tail = r; Edges = l.Prev.Edges |> Map.add (r, l.Prev.Tail) l.Weight }
-
-/// Pseudo Graph Record Type Constructor
-let WeightedPath (inval: 'a) = { Tail = inval; Edges = Map.empty }
+open Edgy.Core
+open Edgy.Utility
 
 /// Combines a sequence of paths, duplicate edges are ignored
-let combine paths = 
+let combineWeightedPaths (paths: WeightedPath<_,_> seq) = 
     paths |> Seq.reduce (fun l r -> { r with Edges = Map.ofSeq [yield! r.Edges |> Map.toSeq; yield! l.Edges |> Map.toSeq] })
 
+/// Given a set of edges, find a set of all nodes
+let allNodes (weightedEdges: (Edge<_>,  _) Map) = weightedEdges |> Seq.collect (fun kvp -> let (l,r) = kvp.Key in [l;r]) |> Set.ofSeq
+
+/// Kruskal's algorithm for undirected weighted graphs.  Will treat all edges are non-directed.
+/// O(NLogN) because of the Sort involved.
+let kruskal (edges: (Edge<_>, _) Map) =
+    let N = edges |> allNodes |> Set.toSeq |> Seq.mapi (fun i n -> n,i) |> Map.ofSeq
+    let rec inner S (V: QuickUWPC) F = 
+        printfn "%A" S
+        match S with
+        | [] -> F
+        | ((a,b), w) :: S -> 
+            let ai, bi = N.[a], N.[b] 
+            printfn "Roots: [%i (%i)] [%i (%i)]" ai (V.Root(ai)) bi (V.Root(bi))             
+            if not <| V.Find(ai,bi) 
+            then V.Union(ai,bi)
+                 inner S V (((a,b),w) :: F)
+            else inner S V F
+    let V = new QuickUWPC(N.Count)
+    let S = edges |> Map.toList |> List.sortBy snd // sort low to high
+    inner S V []
+
 // Example
-let private totalWeightedGraph () =
+let totalWeightedGraph () =
     [
         WeightedPath "A" <=|1.0|= "B" =|0.5|=> "C" <=|0.2|= "D"
         WeightedPath "A" =|0.4|=> "B" =|0.2|=> "C"
-    ] |> combine
+    ] |> combineWeightedPaths
 
 //val toalWeightedGraph : WeightedPath<string,float> =
 //  {Tail = "D";
@@ -52,3 +48,12 @@ let private totalWeightedGraph () =
 //    map
 //      [(("A", "E"), 0.4); (("B", "A"), 1.0); (("B", "C"), 0.5);
 //       (("D", "C"), 0.2); (("E", "D"), 0.2)];}
+
+let kruskaltest () =
+    let graph = 
+        [
+            WeightedPath "A" =|7|=> "B" =|8|=> "C" =|5|=> "E" =|9|=> "G" =|11|=> "F" =|6|=> "D" =|5|=> "A"
+            WeightedPath "D" =|9|=> "B" =|7|=> "E" =|15|=> "D"
+            WeightedPath "E" =|8|=> "F"
+        ] |> combineWeightedPaths
+    printfn "%A" (kruskal (graph.Edges))
