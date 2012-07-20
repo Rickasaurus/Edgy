edb85248fc51ad9166eac92cb216baedce11ba54
Richard Minerich <richard.minerich@gmail.com>
2012-07-20 00:56:34 -0400

    Missed a new file

---
+++ Edgy/Graph.fs
@@ -0,0 +1,15 @@
+﻿module Graph
+
+open Edgy.Core
+
+let toAdjacencyList (graph: IGraph<_,_>) = 
+    graph.Edges 
+    |> Seq.groupBy (fun e -> e.From)
+    |> Seq.map (fun (k,v) -> v |> Seq.map (fun edge -> edge.To))
+    |> Seq.toArray
+
+let toAdjacencyMatrix (graph: IGraph<_,_>) =
+    let arr = Array2D.zeroCreate (graph.NumVertices) (graph.NumVertices)
+    for edge in graph.Edges do
+        arr.[edge.From, edge.To] <- 1
+    arr
